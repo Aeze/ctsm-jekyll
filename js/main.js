ea07ff3a68a0fe296e6d940e0cddd79a9e859b94
Marcus Orochena <marcus.orochena@gmail.com>
2013-10-07 13:46:12 -0400

    added engineering news

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@ var ctsmApp = angular.module("ctsmApp", ["firebase", "ui.tinymce"])
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
 });
 
+ctsmApp.factory('Feed', ['$http', function ($http) {
+  return {
+    parseFeed: function (url) {
+      return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+    }
+  }
+}]);
+
+var FeedCtrl = function($scope, Feed) {
+  Feed.parseFeed('http://feed.eng.umd.edu/news/feed.xml').then(function (res) {
+      $scope.feeds = res.data.responseData.feed.entries;
+   });
+}
+
 function PeopleController($scope, angularFire) {
   var ref = new Firebase('https://ctsm.firebaseio.com/people');
   angularFire(ref, $scope, 'people')
